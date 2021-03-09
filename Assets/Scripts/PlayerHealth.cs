c2a7b51b4e08a1a0bef0c13946d6c2d936cc02c1
Codey Huntting <creativecodey@gmail.com>
2021-03-08 19:17:40 -0800

    Added player health and some player health effects

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerHealthEffects))]
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField]
+    [TagSelector]
+    [Tooltip("Tag of the objects that can hurt the player")]
+    private string hazardTag;
+    [SerializeField]
+    [Tooltip("Health of the player")]
+    private int health;
+    [SerializeField]
+    [Tooltip("Amount of damage dealt to player on each hit")]
+    private int damagePerHit;
+    [SerializeField]
+    [Tooltip("Time for which the player remains invincible after taking damage")]
+    private float invincibilityTime;
+
+    private PlayerHealthEffects effects;
+    private float timeSinceLastHit;
+
+    private void Start()
+    {
+        effects = GetComponent<PlayerHealthEffects>();
+        timeSinceLastHit = invincibilityTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag(hazardTag))
+        {
+            TryTakeDamage();
+        }
+    }
+
+    private void TryTakeDamage()
+    {
+        // If current time exceeds time since last hit plus invincibility time, then take damage
+        if(Time.time > timeSinceLastHit + invincibilityTime)
+        {
+            TakeDamage();
+        }
+    }
+
+    private void TakeDamage()
+    {
+        // Deplete health and set time since last hit to right now
+        health -= damagePerHit;
+        timeSinceLastHit = Time.time;
+
+        if(health <= 0)
+        {
+            // Die!
+        }
+        // If the player isn't dead yet, play the effect for the player taking damage
+        else
+        {
+            effects.TakeDamageEffect();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerHealthEffects.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayerHealthEffects : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Sound effect that plays when the player takes damage")]
+    private AudioClip damageClip;
+
+    // Reference to the audio source that plays effects for the player
+    private AudioSource source;
+
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    public void TakeDamageEffect()
+    {
+        source.clip = damageClip;
+        source.Play();
+
+        // Heart fades in and out while invincible
+    }
+}
