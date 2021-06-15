dcbb5e5039ca96f42e4ec5d72635c6d3ccf49e66
Codey Huntting <creativecodey@gmail.com>
2021-06-15 14:13:52 -0700

    Added invincibility controls displayed off to the side

---
+++ Assets/Scripts/Player/PlayerInvincibilityControlsUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInvincibilityControlsUI : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Transform of the parent with the controls UI")]
+    private Transform controlsParent;
+
+    private void Start()
+    {
+        // Get the controls position in the world
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(controlsParent.position);
+        // Set the y of the world position to the center of the field
+        worldPos.Set(worldPos.x, Field.center.y, worldPos.z);
+        // Convert back to screen point and set the transform
+        controlsParent.position = Camera.main.WorldToScreenPoint(worldPos);
+    }
+}
